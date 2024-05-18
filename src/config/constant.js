bdbbb0bee900fdb12b56ac47b2e20def0d1e0f69
RAHUL2893 <rvtiwari49@gmail.com>
2024-05-18 17:28:20 +0530

    Blood bank app initial commit

---
+++ src/config/constant.js
@@ -0,0 +1,8 @@
+export const BASE_URL = '/app/dashboard/default';
+export const BASE_TITLE = ' | Blood Bank';
+
+export const CONFIG = {
+  layout: 'vertical',
+  collapseMenu: false,
+  layoutType: 'menu-dark'
+};
